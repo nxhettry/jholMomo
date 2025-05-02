757f1e07ba47dfdff8138fc0e7b92435fc143284
nxhettry <nishan.gtsn@gmail.com>
2025-05-02 10:14:58 +0545

    created party page

--- src/app/(bottomnav)/parties/page.tsx
+++ src/app/(bottomnav)/parties/page.tsx
@@ -1,15 +1,8 @@
 "use client";
 
-import { useCallback, useState } from "react";
-import { Plus, Minus, ShoppingBag, Trash } from "lucide-react";
+import { ShoppingBag } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+
 import {
   Card,
   CardContent,
@@ -20,6 +13,28 @@ import {
 import { ArrowLeft } from "lucide-react";
 import { IoSearch } from "react-icons/io5";
 import { Input } from "@/components/ui/input";
+import { IoPersonCircleOutline } from "react-icons/io5";
+
+const PARTIES = [
+  {
+    name: "Party 1",
+    phone: "1234567890",
+    lastUpdated: "2023-10-01",
+    totalOrders: 5,
+  },
+  {
+    name: "Party 2",
+    phone: "1234567890",
+    lastUpdated: "2023-10-01",
+    totalOrders: 5,
+  },
+  {
+    name: "Party 3",
+    phone: "1234567890",
+    lastUpdated: "2023-10-01",
+    totalOrders: 5,
+  },
+];
 
 const Parties = () => {
   return (
@@ -41,26 +56,46 @@ const Parties = () => {
             <CardHeader>
               <CardTitle className="text-lg">
                 <Button className="w-full h-12">Add New Party</Button>
+
                 <p className="text-gray-500 font-normal text-center"> or</p>
+
+                {/* // Search Bar */}
                 <div className="border border-slate-200 rounded-lg p-2 px-3 flex justify-between items-center">
                   <IoSearch className="text-gray-500" />
                   <Input
                     type="text"
                     placeholder="Search Parties ..."
-                    className="border-none font-normal text-sm text-gray-500 focus:ring-0 focus:outline-none focus-visible:ring-0 focus-visible:ring-offset-0 focus-visible:outline-none shadow-none"
+                    className="border-none font-normal text-xs text-gray-500 focus:ring-0 focus:outline-none focus-visible:ring-0 focus-visible:ring-offset-0 focus-visible:outline-none shadow-none"
                   />
                 </div>
               </CardTitle>
             </CardHeader>
             <CardContent className="space-y-4">
-              <div>
-                <label
-                  htmlFor="item-select"
-                  className="block text-sm font-medium mb-2"
+              {PARTIES.map((party, index) => (
+                <div
+                  key={index}
+                  className="flex justify-between items-center p-2 border-b border-slate-200"
                 >
-                  Select Item
-                </label>
-              </div>
+                  <div>
+                    <div className="flex gap-2 items-center">
+                      <IoPersonCircleOutline className="h-12 w-12" />
+                      <div>
+                        <h2 className="text-md font-normal">{party.name}</h2>
+                        <p className="text-xs text-gray-500">
+                          Phone: {party.phone}
+                        </p>
+                      </div>
+                    </div>
+                    <p className="pl-2 text-xs text-gray-500">
+                      Last Order: {party.lastUpdated}
+                    </p>
+                  </div>
+                  <div className="text-right">
+                    <p className="text-lg font-semibold">{party.totalOrders}</p>
+                    <p className="text-xs text-gray-500">Total Orders</p>
+                  </div>
+                </div>
+              ))}
             </CardContent>
             <CardFooter>
               <Button className="w-full">
